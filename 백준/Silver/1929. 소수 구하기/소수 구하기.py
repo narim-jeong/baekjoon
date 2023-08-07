ed3e6808dce1601c0088d1db16898c836a37eb6b
narim-jeong <112361014+narim-jeong@users.noreply.github.com>
2023-08-08 08:58:20 +0900

    [Silver III] Title: 소수 구하기, Time: 3144 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,14 @@
+import sys
+import math
+
+m, n = map(int, sys.stdin.readline().split())
+
+def prime(x):
+    if x == 0 or x == 1: return False
+    for i in range(2, int(math.sqrt(x))+1):
+        if x % i == 0: return False
+    return True
+
+for j in range(m, n+1):
+    if prime(j):
+        print(j)
