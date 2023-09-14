d5d4b04ddbca8e2e1a1c2d16c9630dd67eaa7865
narim-jeong <112361014+narim-jeong@users.noreply.github.com>
2023-09-14 09:46:27 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,16 @@
+import sys
+
+def backtracking():
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+
+    for i in range(1, n+1):
+        if i not in s:
+            s.append(i)
+            backtracking()
+            s.pop()
+
+n, m =map(int,sys.stdin.readline().split())
+s=[]
+backtracking()
